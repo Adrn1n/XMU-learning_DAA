5b4e8a2f22d606d5b236e480a4b084885cf740ab
Exxon <exxonliu001@yahoo.com>
2025-03-22 15:24:21 +0800

    20250322: +: 20250319/1

--- Code_Blocks_projects/XMUOJ/20250319/1/main.cpp
+++ Code_Blocks_projects/XMUOJ/20250319/1/main.cpp
@@ -32,11 +32,62 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define val_MAX 100
 
 using namespace std;
 
+typedef long long idxT;
+typedef long long valT;
+typedef vector<valT> vecValT;
+
+valT getMax_pathVal(vector<vecValT> A)
+{
+    valT maxVal=0;
+    if(!A.empty())
+    {
+        for(auto it=A.begin(); it<A.end(); ++it)
+            if((*it).size()!=(size_t)(it-A.begin()+1))
+                return maxVal;
+        if(A.size()>1)
+            for(auto it1=A.rbegin()+1; it1<A.rend(); ++it1)
+                for(auto it2=(*it1).rbegin(),it=it2; (it2<(*it1).rend())&&(it=(*(it1-1)).rbegin()+(it2-(*it1).rbegin()),true); ++it2)
+                    *it2+=max(*it,*(it+1));
+        maxVal=(A.front()).front();
+    }
+    return maxVal;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    idxT N=0;
+    cin>>N;
+    bool flag=true;
+    if((N>0)&&(!(flag=false)))
+    {
+        vector<vecValT> A(N);
+        for(auto it=A.begin(); it<A.end(); ++it)
+        {
+            for(idxT i=0; i<=(it-A.begin()); ++i)
+            {
+                valT a=0;
+                cin>>a;
+                if((a<0)||(a>val_MAX))
+                {
+                    flag=true;
+                    break;
+                }
+                else
+                    (*it).push_back(a);
+            }
+            if(flag)
+                break;
+        }
+        cout<<getMax_pathVal(A)<<endl;
+    }
+    if(flag)
+        cout<<"ERROR!"<<endl;
     return 0;
 }
