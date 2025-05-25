3a3e5b40601c09bc915a0e5fa5305ae02d35b769
Exxon <exxonliu001@yahoo.com>
2025-05-25 15:18:36 +0800

    20250525: +: 20250521/5

--- Code_Blocks_projects/XMUOJ/20250521/5/main.cpp
+++ Code_Blocks_projects/XMUOJ/20250521/5/main.cpp
@@ -34,11 +34,95 @@ xmu
 */
 
 #include <iostream>
+#include <forward_list>
+#include <queue>
+#include <map>
+
+#define K_MIN 1
+#define K_MAX 12
+#define R_MIN 1
+#define R_MAX 10000
 
 using namespace std;
 
+typedef short idxT;
+typedef int valT;
+
+struct queNode
+{
+    forward_list<valT> Items;
+    valT val;
+};
+
+inline bool isPosible_node(const queNode &node,map<forward_list<valT>,valT> &curState_max)
+{
+    bool flag=false;
+    auto mapIt=curState_max.find(node.Items);
+    if(mapIt==curState_max.end())
+        curState_max[node.Items]=node.val,flag=true;
+    else if(node.val>=(mapIt->second))
+        mapIt->second=node.val,flag=true;
+    return flag;
+}
+
+inline valT getMaxVal_combin2(const forward_list<valT> &A)
+{
+    valT res=0;
+    map<forward_list<valT>,valT> curState_max;
+    queue<queNode> Que;
+    Que.push({A,0});
+    while(!Que.empty())
+    {
+        auto &node=Que.front();
+        if(distance((node.Items).begin(),(node.Items).end())>1)
+        {
+            if(isPosible_node(node,curState_max))
+            {
+                auto &items=node.Items;
+                for(auto it=items.begin(); it!=items.end(); ++it)
+                {
+                    auto next_it=next(it);
+                    if(next_it!=items.end())
+                    {
+                        auto tmp=*next_it;
+                        (*it)+=tmp,items.erase_after(it),node.val+=(*it);
+                        if(isPosible_node(node,curState_max))
+                            Que.push(node);
+                        node.val-=(*it),(*it)-=tmp,items.insert_after(it,tmp);
+                    }
+                }
+            }
+        }
+        else
+            res=(node.val>res)?(node.val):res;
+        Que.pop();
+    }
+    return res;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    idxT k=0;
+    cin>>k;
+    bool flag=true;
+    if((k>=K_MIN)&&(k<=K_MAX))
+    {
+        forward_list<valT> A(k);
+        for(auto &r:A)
+        {
+            cin>>r;
+            if((r<R_MIN)||(r>R_MAX))
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(flag)
+            cout<<getMaxVal_combin2(A)<<endl,flag=false;
+        else
+            flag=true;
+    }
+    if(flag)
+        cout<<"ERROR!"<<endl;
     return 0;
 }
